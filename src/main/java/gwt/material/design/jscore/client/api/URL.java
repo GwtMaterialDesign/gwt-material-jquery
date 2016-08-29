595fe49f299ffd3119a9707494da9fc6984e49fc
Kevzlou7979 <kevzlou7979@gmail.com>
2016-08-29 22:03:19 +0800

    Removed unused imports

--- src/main/java/gwt/material/design/jscore/client/api/URL.java
+++ src/main/java/gwt/material/design/jscore/client/api/URL.java
@@ -1,8 +1,5 @@
 package gwt.material.design.jscore.client.api;
 
-import elemental2.Blob;
-import elemental2.MediaSource;
-import elemental2.URLSearchParams;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
@@ -19,14 +16,9 @@ public class URL {
     public String port;
     public String protocol;
     public String search;
-    public URLSearchParams searchParams;
     public String username;
 
     public static native String createObjectURL(MediaStream var0);
-
-    public static native String createObjectURL(Blob var0);
-
-    public static native String createObjectURL(MediaSource var0);
 
     public static native String domainToASCII(String var0);
 
