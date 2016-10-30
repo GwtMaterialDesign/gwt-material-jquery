a7f2658fc1d17424c3cbb9c8898fdd7182657c4b
BenDol <dolb90@gmail.com>
2016-10-31 12:57:11 +1300

    Remove our license information.

--- src/main/java/gwt/material/design/jscore/client/JSNI.java
+++ src/main/java/gwt/material/design/jscore/client/JSNI.java
@@ -1,32 +1,9 @@
-package gwt.material.design.jscore.client;
-
-/*
- * #%L
- * GwtMaterial
- * %%
- * Copyright (C) 2015 - 2016 GwtMaterialDesign
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package gwt.material.design.jscore.client;
 
 import gwt.material.design.jscore.client.api.Console;
 import gwt.material.design.jscore.client.api.JSON;
@@ -38,16 +15,15 @@ import gwt.material.design.jscore.client.api.Window;
  */
 public class JSNI {
     
-    public final static native Console createConsole() /*-{
+    public static native Console createConsole() /*-{
         return $wnd.console;
     }-*/;
     
-    public final static native Window createWindow() /*-{
+    public static native Window createWindow() /*-{
         return $wnd;
     }-*/;
     
-    public final static native JSON createJSON() /*-{
+    public static native JSON createJSON() /*-{
         return $wnd.JSON;
     }-*/;
-    
 }
--- src/main/java/gwt/material/design/jscore/client/api/html/HTMLElement.java
+++ src/main/java/gwt/material/design/jscore/client/api/html/HTMLElement.java
@@ -15,27 +15,6 @@
  */
 package gwt.material.design.jscore.client.api.html;
 
-/*
- * #%L
- * GwtMaterial
- * %%
- * Copyright (C) 2015 - 2016 GwtMaterialDesign
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
-
 import gwt.material.design.jscore.client.api.core.Element;
 import gwt.material.design.jscore.client.api.core.EventListener;
 import gwt.material.design.jscore.client.api.JsObject;
@@ -52,18 +31,18 @@ import jsinterop.annotations.JsType;
 public class HTMLElement extends Element {
 
     @JsMethod
-    public static native void setAttribute(String align, String center);
+    public native void setAttribute(String align, String center);
 
     @JsMethod
-    public static native void appendChild(HTMLElement element);
+    public native void appendChild(HTMLElement element);
 
     @JsMethod
-    public static native void addEventListener(String event, EventListener<? extends JsObject> handler);
+    public native void addEventListener(String event, EventListener<? extends JsObject> handler);
 
     @JsProperty
-    public static native void setInnerText(String text);
+    public native void setInnerText(String text);
  
     @JsProperty
-    public static native String getInnerText();
+    public native String getInnerText();
 
 }
